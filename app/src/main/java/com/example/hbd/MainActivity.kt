8c11c55be0dd4ae919c51bf6dae84af7346108b2
ASUSAL\alriz <alriskialma@gmail.com>
2024-10-17 14:27:52 +0700

    pertemuan6

--- app/src/main/java/com/example/hbd/MainActivity.kt
+++ app/src/main/java/com/example/hbd/MainActivity.kt
@@ -4,14 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.hbd.ui.theme.HbdTheme
 
@@ -36,12 +40,20 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    // Menggunakan Box untuk memposisikan konten di tengah
-    Box(
-        contentAlignment = Alignment.Center, // Posisikan teks di tengah
-        modifier = modifier
+    // Memanggil gambar dari drawable
+    val image: Painter = painterResource(id = R.drawable.mrbean) // Ganti 'your_image' dengan nama file gambar di drawable
+
+    // Menggunakan Column untuk menyusun gambar dan teks secara vertikal
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally, // Posisikan konten di tengah
+        modifier = modifier.fillMaxSize()
     ) {
-        // Ubah warna teks dan ukuran font di sini
+        Image(
+            painter = image,
+            contentDescription = "Birthday Image", // Tambahkan deskripsi gambar
+            modifier = Modifier.size(300.dp) // Atur ukuran gambar sesuai kebutuhan
+        )
+        Spacer(modifier = Modifier.height(16.dp)) // Spasi antara gambar dan teks
         Text(
             text = "Happy Birthday $name!",
             color = Color.Red, // Ubah warna teks
